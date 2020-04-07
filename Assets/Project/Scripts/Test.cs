d708c4301a44643f521322a78613461a38101abc
Justin Wong <jusw85@hotmail.com>
2020-04-07 14:03:57 +0800

    Initial commit

---
+++ Assets/Project/Scripts/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] private UnityEvent destroyedOther;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
