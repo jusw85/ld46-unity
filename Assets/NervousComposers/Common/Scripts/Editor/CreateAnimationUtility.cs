80f98b887e6b296538584310bbc144de89f32505
Justin Wong <jusw85@hotmail.com>
2020-04-14 16:42:17 +0800

    Add animation utility

---
+++ Assets/NervousComposers/Common/Scripts/Editor/CreateAnimationUtility.cs
@@ -0,0 +1,99 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Jusw85.Common
+{
+    public class CreateAnimationUtility : EditorWindow
+    {
+        private AnimationClip clip;
+        private int targetFrameInterval;
+
+        [MenuItem("Tools/Nervous Composers/Create Animation Utility")]
+        public static void CreateAll()
+        {
+            GetWindow(typeof(CreateAnimationUtility));
+        }
+
+        private void OnGUI()
+        {
+            GUILayout.Label("Set clip timings");
+            clip = (AnimationClip) EditorGUILayout.ObjectField("Animation Clip", clip, typeof(AnimationClip), false);
+            targetFrameInterval = EditorGUILayout.IntField("Num frames between sprites", targetFrameInterval);
+            if (GUILayout.Button("Set clip timings"))
+            {
+                SetClipTimings();
+            }
+        }
+
+        private void SetClipTimings()
+        {
+            targetFrameInterval = (int) Mathf.Clamp(targetFrameInterval, 1f, int.MaxValue);
+            if (clip == null)
+            {
+                Debug.Log("No clip selected");
+                return;
+            }
+
+            EditorCurveBinding? spriteCurveBindingOpt = getSpriteCurveBinding(clip);
+            if (!spriteCurveBindingOpt.HasValue)
+            {
+                Debug.Log("Unable to find sprite curve");
+                return;
+            }
+
+            EditorCurveBinding spriteCurveBinding = spriteCurveBindingOpt.Value;
+
+            ObjectReferenceKeyframe[]
+                keyFrames = AnimationUtility.GetObjectReferenceCurve(clip, spriteCurveBinding);
+            int l = keyFrames.Length;
+            bool lastFrameRepeated = l > 1 && (keyFrames[l - 1].value == keyFrames[l - 2].value);
+            int numSprites = lastFrameRepeated ? l - 1 : l;
+            bool repeatLastFrame = numSprites > 1 && targetFrameInterval > 1;
+            float frameRate = clip.frameRate;
+            float frameTime = 1f / frameRate;
+
+            ObjectReferenceKeyframe[] newKeyFrames =
+                new ObjectReferenceKeyframe[repeatLastFrame ? numSprites + 1 : numSprites];
+            int i = 0;
+            ObjectReferenceKeyframe keyFrame;
+            for (; i < numSprites; i++)
+            {
+                keyFrame = new ObjectReferenceKeyframe
+                {
+                    value = keyFrames[i].value,
+                    time = (i * targetFrameInterval) * frameTime
+                };
+                newKeyFrames[i] = keyFrame;
+            }
+
+            if (repeatLastFrame)
+            {
+                keyFrame = new ObjectReferenceKeyframe
+                {
+                    value = keyFrames[i - 1].value,
+                    time = ((i * targetFrameInterval) - 1) * frameTime
+                };
+                newKeyFrames[i] = keyFrame;
+            }
+
+            AnimationUtility.SetObjectReferenceCurve(clip, spriteCurveBinding, newKeyFrames);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+
+        private EditorCurveBinding? getSpriteCurveBinding(AnimationClip clip)
+        {
+            EditorCurveBinding[] objectCurveBindings = AnimationUtility.GetObjectReferenceCurveBindings(clip);
+            IEnumerator<EditorCurveBinding> spriteCurveBindings =
+                objectCurveBindings.Where(obj => obj.propertyName.Equals("m_Sprite")).GetEnumerator();
+            if (!spriteCurveBindings.MoveNext())
+            {
+                return null;
+            }
+
+            return spriteCurveBindings.Current;
+        }
+    }
+}
