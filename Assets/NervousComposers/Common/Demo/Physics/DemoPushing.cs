040c8cc9f14560ef898c723991151cdffa794e5d
Justin Wong <jusw85@hotmail.com>
2020-04-17 21:30:55 +0800

    Add demo pushing

--- Assets/NervousComposers/Common/Demo/Physics/DemoPushing.cs
+++ Assets/NervousComposers/Common/Demo/Physics/DemoPushing.cs
@@ -1,26 +1,29 @@
 ﻿using UnityEngine;
 
-public class DemoPushing : MonoBehaviour
+namespace Jusw85.Common.Demo
 {
-    private Rigidbody2D rb;
-
-    public Vector2 velocity;
-
-    void Start()
+    public class DemoPushing : MonoBehaviour
     {
-        Physics2D.gravity = Vector2.zero;
-        Physics2D.SetLayerCollisionMask(LayerMask.NameToLayer("Default"), LayerMask.GetMask("Default"));
-        rb = GetComponent<Rigidbody2D>();
-    }
+        private Rigidbody2D rb;
 
-    void FixedUpdate()
-    {
-        rb.velocity = velocity;
-    }
+        public Vector2 velocity;
 
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        Debug.Log("OnCollisionEnter2D");
-        velocity = Vector2.zero;
+        void Start()
+        {
+            Physics2D.gravity = Vector2.zero;
+            Physics2D.SetLayerCollisionMask(LayerMask.NameToLayer("Default"), LayerMask.GetMask("Default"));
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        void FixedUpdate()
+        {
+            rb.velocity = velocity;
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            Debug.Log("OnCollisionEnter2D");
+            velocity = Vector2.zero;
+        }
     }
 }
